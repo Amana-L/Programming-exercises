ba9bf372cf6d0727d2e666adaacc5e196a8206bd
Amana-L <64684829+Amana-L@users.noreply.github.com>
2023-06-19 13:11:14 +0100

    Update test_checkout.py
    Added further tests

--- kata-catalog/test_checkout.py
+++ kata-catalog/test_checkout.py
@@ -6,15 +6,33 @@ class TestCheckout(unittest.TestCase):
     def setUp(self):
         self.checkout = Checkout()
 
+    def test_scan_nothing(self):
+        result = self.checkout.scan('')               
+        result = self.checkout.total()
+        self.assertEqual(result, 'Total: $0.00')
+
     def test_scan_invalid_sku(self):
         result = self.checkout.scan('INVALID', 1)
         self.assertEqual(result, 'Invalid item SKU')
 
-    def test_total(self):
-        self.checkout.scan('SOUP', 2)
-        self.checkout.scan('RAMEN', 3)
+    def test_scan_one(self):
+        self.checkout.scan('RAMEN', 1)
         result = self.checkout.total()
-        self.assertEqual(result, 'Total: $2.00')
+        self.assertEqual(result, 'Total: $0.40')
+
+    def test_multiple(self):
+        self.checkout.scan('SOUP', 1)
+        self.checkout.scan('RAMEN', 1)
+        result = self.checkout.total()
+        self.assertEqual(result, 'Total: $1.40')
+
+    def test_multiple_with_all_discounts(self):
+        self.checkout.scan('SOUP', 2)
+        self.checkout.scan('YOGURT', 3)
+        self.checkout.scan('RAMEN', 3)
+        self.checkout.scan('ORANGE', 1)
+        result = self.checkout.total()
+        self.assertEqual(result, 'Total: $8.00')
 
 if __name__ == '__main__':
     unittest.main()
